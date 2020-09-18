624cc053394859703482c4c4fd0cbb1f86d23b65
Dominik Jurasz <jurasz.do@gmail.com>
2020-09-18 10:16:56 +0200

    Already added fine config resolution

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
+
 const envFound = dotenv.config();
-console.log('config load');
 if (!envFound) {
   // This error should crash whole process
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
